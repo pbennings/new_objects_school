c7c72d8998925f58027d7846ae83d4db61c12f51
Paul Bennings <>
2019-05-28 14:35:44 -0400

    finishing up code

--- objects.js
+++ objects.js
@@ -1,15 +1,21 @@
-function School(branch, term, student) {
+function School(branch, term, student, subjects) {
   this.branch = branch;
   this.term = term;
   this.student = student;
+  this.subjects = subjects;
 }
+
+let school = new School
+console.log(subjects);
 
 let classroom = new Object();
 classroom.class = "code";
 classroom.room = 214;
 classroom.ac = false;
+classroom.heat = true;
+classroom.subjects = 'GIT';
 console.log(classroom);
 
-let person1 = new School("code immersives", "Term 2", "Paul");
+let person1 = new School("code immersives", "Term 2", "Paul", 'GIT');
 
-console.log();
+console.log(classroom);
