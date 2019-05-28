469b350de595e1f50a55cab16c0b563967600b6b
Paul Bennings <>
2019-05-28 14:53:08 -0400

    finished?

--- objects.js
+++ objects.js
@@ -1,21 +1,20 @@
-function School(branch, term, student, subjects) {
+function Student(branch, term, student, subjects) {
   this.branch = branch;
   this.term = term;
   this.student = student;
   this.subjects = subjects;
 }
 
-let school = new School
-console.log(subjects);
+let person1 = new Student("code immersives", "Term 2", "Paul", "GIT");
+let person2 = new Student("code immersives", "term 1", "James", "CSS");
+console.log(person1, person2);
 
-let classroom = new Object();
-classroom.class = "code";
-classroom.room = 214;
-classroom.ac = false;
-classroom.heat = true;
-classroom.subjects = 'GIT';
-console.log(classroom);
-
-let person1 = new School("code immersives", "Term 2", "Paul", 'GIT');
-
-console.log(classroom);
+// let classroom = new Object();
+// classroom.class = "code";
+// classroom.room = 214;
+// classroom.ac = false;
+// classroom.heat = true;
+// classroom.subjects = "GIT";
+// classroom.student1 = person1;
+// classroom.student2 = person2;
+// console.log(classroom);
